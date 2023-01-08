facfc0a681fa24b551a9cabd1b701a99eacc5861
Andy O'Neill <aoneill01@gmail.com>
2023-01-08 09:49:07 -0500

    Day 1

--- src/days/day1.js
+++ src/days/day1.js
@@ -1,9 +1,80 @@
-import { getInput } from "../helpers/getInput.js"
+import { getInput } from '../helpers/getInput.js'
+
+const north = {
+  dx: 0,
+  dy: -1,
+}
+
+const east = {
+  dx: 1,
+  dy: 0,
+}
+
+const south = {
+  dx: 0,
+  dy: 1,
+}
+
+const west = {
+  dx: -1,
+  dy: 0,
+}
+
+north.right = east
+north.left = west
+
+east.right = south
+east.left = north
+
+south.right = west
+south.left = east
+
+west.right = north
+west.left = south
 
 export async function part1() {
-  // const input = await getInput(1)
+  const input = (await getInput(1))[0].split(', ')
+
+  let x = 0,
+    y = 0,
+    dir = north
+
+  for (const instruction of input) {
+    if (instruction[0] === 'R') dir = dir.right
+    else dir = dir.left
+
+    x += dir.dx * +instruction.substring(1)
+    y += dir.dy * +instruction.substring(1)
+  }
+
+  console.log(Math.abs(x) + Math.abs(y))
 }
 
 export async function part2() {
-  // const input = await getInput(1)
+  const input = (await getInput(1))[0].split(', ')
+
+  let x = 0,
+    y = 0,
+    dir = north
+
+  const visited = new Set()
+  visited.add(`${x},${y}`)
+
+  outer: for (const instruction of input) {
+    if (instruction[0] === 'R') dir = dir.right
+    else dir = dir.left
+
+    for (let i = 0; i < +instruction.substring(1); i++) {
+      x += dir.dx
+      y += dir.dy
+
+      if (visited.has(`${x},${y}`)) {
+        break outer
+      }
+
+      visited.add(`${x},${y}`)
+    }
+  }
+
+  console.log(Math.abs(x) + Math.abs(y))
 }
--- src/helpers/getInput.js
+++ src/helpers/getInput.js
@@ -1,24 +1,37 @@
+import { access, readFile, writeFile } from 'fs/promises'
+import { constants } from 'fs'
 import fetch from 'node-fetch'
 import dotenv from 'dotenv'
 dotenv.config()
 
 export async function getInput(day) {
-  const response = await fetch(`https://adventofcode.com/2015/day/${day}/input`, {
-    headers: {
-      accept: 'text/html',
-      cookie: process.env.COOKIE,
-    },
-    method: 'GET',
-  })
+  const filename = `cache/${day}.txt`
+  let text
 
-  const text = await response.text()
+  try {
+    await access(filename, constants.R_OK)
+
+    text = await readFile(filename, { encoding: 'utf8' })
+  } catch {
+    const response = await fetch(`https://adventofcode.com/2016/day/${day}/input`, {
+      headers: {
+        accept: 'text/html',
+        cookie: process.env.COOKIE,
+      },
+      method: 'GET',
+    })
+
+    text = await response.text()
+
+    await writeFile(filename, text)
+  }
 
   const lines = text.split('\n')
   return lines[lines.length - 1] === '' ? lines.slice(0, lines.length - 1) : lines
 }
 
 export async function* getInputAsyncGenerator(day) {
-  const response = await fetch(`https://adventofcode.com/2015/day/${day}/input`, {
+  const response = await fetch(`https://adventofcode.com/2016/day/${day}/input`, {
     headers: {
       accept: 'text/html',
       cookie: process.env.COOKIE,
